6b2fbaef6c44f940e4aaf16c4b38253e87e52b03
Lucas Eduardo <lucas.eduardo.developer@gmail.com>
2020-10-02 11:56:23 -0300

    SEO component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
+import SEO from '../components/SEO';
 import { Title } from '../styles/pages/Home';
 
 export default function Home() {
   return (
     <div>
+      <SEO
+        title="DevCommerce, your best e-commerce!"
+        image="boost.png"
+        shouldExcludeTitleSuffix
+      />
+
       <Title>Hello World</Title>
     </div>
   )
