e71e7032506a4d08bf0a4e2bb630fb8d6fd6ce48
Lucas Eduardo <lucas.eduardo.developer@gmail.com>
2020-10-02 11:12:38 -0300

    dynamic import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
+import { useCallback } from 'react';
 import { Title } from '../styles/pages/Home';
 
 export default function Home() {
+  const handleSum = useCallback(async () => {
+    const { sum } = (await import('../lib/math')).default;
+
+    alert(sum(1, 5));
+  }, []);
+
   return (
     <div>
       <Title>Hello World</Title>
+
+      <button onClick={handleSum}>Sum!</button>
     </div>
   )
 }
