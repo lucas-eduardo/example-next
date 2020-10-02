37bdd36c40e8ebfa4634b63c6fd714622004c84d
Lucas Eduardo <lucas.eduardo.developer@gmail.com>
2020-10-02 11:32:49 -0300

    environment variables

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,40 @@
+import { GetServerSideProps } from 'next';
 import { Title } from '../styles/pages/Home';
 
-export default function Home() {
+interface IProduct {
+  id: string;
+  title: string;
+}
+
+interface HomeProps {
+  recommendedProducts: IProduct[]
+}
+
+export default function Home({ recommendedProducts }: HomeProps) {
   return (
     <div>
-      <Title>Hello World</Title>
+      <section>
+        <Title>Products</Title>
+
+        <ul>
+          {recommendedProducts.map((recommendedProduct) => (
+            <li key={recommendedProduct.id}>
+              {recommendedProduct.title}
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/recommended`);
+  const recommendedProducts = await response.json();
+
+  return {
+    props: {
+      recommendedProducts
+    }
+  }
 }
